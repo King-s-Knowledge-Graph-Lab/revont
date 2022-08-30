aa2d612db1420f22aab33deffa155fa88a953133
Fiorela Ciroku <fiorelaciroku@yahoo.com>
2022-08-30 18:27:26 +0200

    Update questionGeneration.py

--- Scripts/questionGeneration.py
+++ Scripts/questionGeneration.py
@@ -1,20 +1,17 @@
-# Install transformers 
+#install transformers 
 !pip install transformers
-# Install sentencepiece
+#install sentencepiece
 !pip install sentencepiece
 
+import json
 from transformers import AutoModelWithLMHead, AutoTokenizer
 
-# Import JSON
-import JSON
-
-# T5 model for question generation
 tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
 model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
 
-# Define function for generating questions
+# Function to generate questions using the T5 model
 def get_question(answer, context, max_length=64):
-  input_text = "answer: %s  context: %s </s>" % (answer, context)
+  input_text = "answer: %s  context: %s " % (answer, context)
   features = tokenizer([input_text], return_tensors='pt')
 
   output = model.generate(input_ids=features['input_ids'], 
@@ -22,44 +19,47 @@ def get_question(answer, context, max_length=64):
                max_length=max_length)
 
   return tokenizer.decode(output[0])
-  
-# Opening and load JSON file
+
+# Function to "refine" the generated question
+
+def refine_question(question):
+    j = question.replace('<pad> question: ', '')
+    k = j.replace('</s>', '')
+    return(k)
+
+# Open and load dataset JSON file
 f = open('/content/drive/MyDrive/KCL experiment/WDV_JSON.json')
 data = json.load(f)
 
-# Iterate through the dataset
-with open('/content/drive/MyDrive/KCL experiment/Themed questions/Street.json', "r+") as file:
+# Generate questions when providing the property or the object as answer and the verbalization as context
+with open('/content/drive/MyDrive/KCL experiment/Questions.json', "r+") as file:
   output = json.load(file)
   for doc in data:
-    if doc['theme_label'] == 'Street':
-      subjectCQ = get_question(doc['subject_label'], doc['verbalisation_unk_replaced'])
-      propertyCQ = get_question(doc['property_label'], doc['verbalisation_unk_replaced'])
-      objectCQ = get_question(doc['object_label'], doc['verbalisation_unk_replaced'])
+    propertyCQ = get_question(doc['property_label'], doc['verbalisation_unk_replaced'])
+    objectCQ = get_question(doc['object_label'], doc['verbalisation_unk_replaced'])
   
-      # Create JSON entry
-      entry = {"claim_id": doc['claim_id'],
-           "SAnswer":{
-           "subject_label": doc['subject_label'],
-           "context": doc['verbalisation_unk_replaced'],
-           "subjectCQ": subjectCQ
-           },
+     # Create JSON entry with new questions
+    entry = {
+           "claim_id": doc['claim_id'],
+           "theme": doc['theme_label'],
            "PAnswer":{
            "property_label": doc['property_label'],
            "context": doc['verbalisation_unk_replaced'],
-           "propertyCQ": propertyCQ 
+           "propertyCQ": refine_question(propertyCQ) 
            },
            "OAnswer": {
            "object_label": doc['object_label'],
            "context": doc['verbalisation_unk_replaced'],
-           "objectCQ": objectCQ
+           "objectCQ": refine_question(objectCQ)
            }
           }
-
-    # Append the entry in the file
-      output.append(entry)
-      file.seek(0)
-      json.dump(output, file)
+    # Append the entry to the new JSON file
+    output.append(entry)
+    file.seek(0)
+    json.dump(output, file)
+  
 
 # Closing file
 f.close()
 output.close()
+
